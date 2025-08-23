21174a75a208b730b9cd16885f2f293305dc2d8b
Ken <kenvu@pm.me>
2025-08-23 14:04:19 +0100

    - Created the BotcPack

---
+++ src/main/java/com/botcfab/BotcPack.java
@@ -0,0 +1,25 @@
+package com.botcfab;
+
+import net.minecraft.block.Block;
+
+import java.util.ArrayList;
+
+public class BotcPack extends Block {
+    public BotcPack(Settings settings) {
+        super(settings);
+    }
+    public ArrayList<BotcRole> allPossibleRoles = new ArrayList<BotcRole>();
+
+    public ArrayList<BotcRole> getAllEvilPlayers() {
+        return null;
+    }
+    public ArrayList<BotcRole> getAllDemons() {
+        return null;
+    }
+    public ArrayList<BotcRole> getAllTownsfolk() {
+        return null;
+    }
+    public ArrayList<BotcRole> getAllOutsiders() {return null;}
+
+    public setPack
+}
