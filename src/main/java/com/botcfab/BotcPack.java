7b379f952af3c65b63f96254fbfade69a69d6a15
Ken <kenvu@pm.me>
2025-08-24 23:02:40 +0100

    Updated BotcPack class to actually do stuff with the functions

--- src/main/java/com/botcfab/BotcPack.java
+++ src/main/java/com/botcfab/BotcPack.java
@@ -2,24 +2,61 @@ package com.botcfab;
 
 import net.minecraft.block.Block;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class BotcPack extends Block {
-    public BotcPack(Settings settings) {
-        super(settings);
+public class BotcPack{
+    private ArrayList<BotcRolesEnum> roles;
+    /// mappedRoles provide a list of roles categorised by the team names
+    /// We are using Hashmap to provide faster lookup times
+    private HashMap<String, ArrayList<BotcRole>> mappedRoles = new HashMap<>();
+
+    public BotcPack(ArrayList<BotcRolesEnum> listOfRoles) {
+        this.roles = listOfRoles;
+        for (BotcRolesEnum roleEnum: listOfRoles) {
+            BotcRole role = BotcRoleCacher.getRole(roleEnum.getId());
+            if (role != null) {
+                ArrayList<BotcRole> tempRoles = this.mappedRoles.getOrDefault(role.getTeam(), new ArrayList<>());
+                tempRoles.add(role);
+                this.mappedRoles.put(role.getTeam(), tempRoles);
+            }
+        }
     }
-    public ArrayList<BotcRole> allPossibleRoles = new ArrayList<BotcRole>();
 
     public ArrayList<BotcRole> getAllEvilPlayers() {
-        return null;
+        ArrayList<BotcRole> tempRole = new ArrayList<>();
+        tempRole.addAll(mappedRoles.get("minion"));
+        tempRole.addAll(mappedRoles.get("demon"));
+        return tempRole;
     }
     public ArrayList<BotcRole> getAllDemons() {
-        return null;
+        return mappedRoles.get("demon");
+    }
+
+    public ArrayList<BotcRole> getAllMinions() {
+        return mappedRoles.get("minion");
     }
     public ArrayList<BotcRole> getAllTownsfolk() {
-        return null;
+        return mappedRoles.get("townsfolk");
     }
-    public ArrayList<BotcRole> getAllOutsiders() {return null;}
+    public ArrayList<BotcRole> getAllOutsiders() {
+        return mappedRoles.get("outsider");
+    }
 
-    public setPack
+    public ArrayList<BotcRole> getAllGoodRoles() {
+        ArrayList<BotcRole> tempRole = new ArrayList<>();
+        tempRole.addAll(mappedRoles.get("townsfolk"));
+        tempRole.addAll(mappedRoles.get("outsider"));
+        return tempRole;
+    }
+
+    public void removeRole(BotcRole selectedRole) {
+        for (String key: mappedRoles.keySet()) {
+            mappedRoles.get(key).remove(selectedRole);
+            BotcFab.LOGGER.info("Removed mappedRole {}", selectedRole.getName());
+            BotcFab.LOGGER.info("mappedRoles are now {}", mappedRoles.toString());
+        }
+    }
+
 }
