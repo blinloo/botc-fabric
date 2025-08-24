4265504b822bfa0db62c4f9032c40c267410fe86
Ken <kenvu@pm.me>
2025-08-24 09:40:44 +0100

    - Created BotcRoleCacher - Optimised role cacher for O(1) lookups

---
+++ src/main/java/com/botcfab/BotcRoleCacher.java
@@ -0,0 +1,39 @@
+package com.botcfab;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class BotcRoleCacher {
+    private static HashMap<String, BotcRole> cachedRoles = new HashMap<>();
+    static {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            // Read JSON file into a list of roles
+            ArrayList<BotcRole> data = mapper.readValue(new File("all-possible-roles.json"), mapper.getTypeFactory().constructCollectionType(List.class, BotcRole.class));
+            BotcFab.LOGGER.info("all-possible-roles successfully cached (number of roles cached: {})", data.size());
+
+            // Populate the Hasmap for faster lookups
+            for (BotcRole role: data) {
+                cachedRoles.put(role.getId(), role);
+            }
+        } catch (IOException e) {
+            BotcFab.LOGGER.info("Failed to cache all roles. Please check your configuration");
+            BotcFab.LOGGER.debug("Due to {}", e.toString());
+        }
+    }
+
+    /**
+     * Retrieve a role by its id value.
+     * BotcRolesEnum contains a mapping of enums to ID.
+     * @param roleId The unique string ID of the role.
+     * @return The BotcRole object or null if not found.
+     */
+    public static BotcRole getRole(String roleId) {
+        return cachedRoles.get(roleId);
+    }
+}
